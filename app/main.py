34dde46fa0018e74d560fdffe2f77cc7ded98547
veeratnj <veerakumarhgkfm@gmail.com>
2026-04-21 13:00:53 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -60,7 +60,7 @@ app = FastAPI(
 # Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOWED_HOSTS,
+    allow_origins=["*"],  # In production, specify allowed origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
